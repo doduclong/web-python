0daf6bfbd0bb4da4ad712f98904cd424060c5b48
doduclong <67698939+doduclong@users.noreply.github.com>
2021-11-09 10:02:15 +0700

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -19,13 +19,14 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .models import User #, Post, Comment
+    from .models import User
 
     create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
     login_manager.init_app(app)
+    login_manager.login_message = "Vui lòng đăng nhập"
 
     @login_manager.user_loader
     def load_user(id):
