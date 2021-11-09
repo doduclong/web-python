b41c8de22dcad6b9b236fefe52eaf00bf46d5361
doduclong <67698939+doduclong@users.noreply.github.com>
2021-11-09 10:02:51 +0700

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -16,13 +16,13 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash("Logged in!", category='success')
+                flash("Đăng nhập thành công", category='success')
                 login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Password is incorrect.', category='error')
+                flash('Sai mật khẩu', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Email không tồn tại', category='error')
 
     return render_template("login.html", user=current_user)
 
@@ -39,24 +39,24 @@ def sign_up():
         username_exists = User.query.filter_by(username=username).first()
 
         if email_exists:
-            flash('Email is already in use.', category='error')
+            flash('Email đã được sử dụng', category='error')
         elif username_exists:
-            flash('Username is already in use.', category='error')
+            flash('Tên đã được sử dụng', category='error')
         elif password1 != password2:
-            flash('Password don\'t match!', category='error')
+            flash('Mật khẩu xác nhận sai', category='error')
         elif len(username) < 2:
-            flash('Username is too short.', category='error')
+            flash('Tên quá ngắn', category='error')
         elif len(password1) < 6:
-            flash('Password is too short.', category='error')
+            flash('Mật khẩu quá ngắn', category='error')
         elif len(email) < 4:
-            flash("Email is invalid.", category='error')
+            flash("Email không hợp lệ", category='error')
         else:
             new_user = User(email=email, username=username, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('User created!')
+            flash('Tạo tài khoản thành công!')
             return redirect(url_for('views.home'))
 
     return render_template("signup.html", user=current_user)
